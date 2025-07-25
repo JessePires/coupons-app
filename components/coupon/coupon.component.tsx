524501cba329555d6d438d2a789b526b5eea5882
JessePires <jessepires2010@gmail.com>
2025-07-24 22:12:12 -0300

    fix(coupon): create coupons component

--- components/coupon/coupon.component.tsx
+++ components/coupon/coupon.component.tsx
@@ -9,29 +9,29 @@ const Coupon = (): JSX.Element => {
   return (
     <View className="flex flex-row">
       <View>
-        <CouponLeftBg />
+        <CouponLeftBg height="120" width="70" />
       </View>
       <View className="w-72 rounded-r-2xl border-slate-400 bg-white p-3">
         <View className="flex-row justify-between">
           <View>
-            <Text className="text-base">Bella Napoli Pizzaria</Text>
-            <Text className="text-xs font-medium">15% OFF em pedidos acima de R$50</Text>
+            <Text className="text-[12px]">Bella Napoli Pizzaria</Text>
+            <Text className="text-[8px] font-medium">15% OFF em pedidos acima de R$ 50</Text>
           </View>
-          <View className="bg-customOrange-500 size-10 rounded-full border-[1px] border-gray-300"></View>
+          <View className="size-10 rounded-full border-[1px] border-gray-300 bg-customOrange-500"></View>
         </View>
-        <View className="flex-row gap-1 pb-3 pt-2">
-          <Tag className="border-customOrange-300 bg-customOrange-200 h-7 w-[60px]">
-            <Text className="text-customOrange-600 text-[8px] font-bold">Restaurante</Text>
+        <View className="flex-row gap-1 pb-1 pt-1">
+          <Tag className="h-7 w-[60px] border-customOrange-300 bg-customOrange-200">
+            <Text className="text-[8px] font-bold text-customOrange-600">Restaurante</Text>
           </Tag>
-          <Tag className="border-customOrange-300 bg-customOrange-200 h-7 w-[60px] flex-row gap-1">
+          <Tag className="h-7 w-[60px] flex-row gap-1 border-customOrange-300 bg-customOrange-200">
             <LocalIcon />
-            <Text className="text-customOrange-600 text-[8px] font-bold">A 850m</Text>
+            <Text className="text-[8px] font-bold text-customOrange-600">A 850m</Text>
           </Tag>
         </View>
         <View className="flex flex-row items-end justify-between">
-          <Text className="text-sm text-gray-500">Válido até 22/07</Text>
-          <TouchableOpacity className="h-8 w-28 items-center justify-center rounded-full border-[1px] border-gray-300 bg-white">
-            <Text className="text-sm font-semibold text-gray-700">Ver Cupom</Text>
+          <Text className="text-[8px] text-gray-500">Válido até 22/07</Text>
+          <TouchableOpacity className="h-8 w-24 items-center justify-center rounded-full border-[1px] border-gray-300 bg-white">
+            <Text className="text-[10px] font-semibold text-gray-700">Ver Cupom</Text>
           </TouchableOpacity>
         </View>
       </View>
